45c4a589437f54caa363e58582124e4585181ce4
luca-macedone <macedone.luca@gmail.com>
2023-03-08 12:01:21 +0100

    add bonus version

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,21 @@ Decrizione passaggi:
     - Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
 */
 const suggestedPassword = document.getElementById("suggested-password");
+// V2 with form
+const submitBtn = document.getElementById("submit-btn");
 
-const userName = prompt("What's your name?");
-const userSurname = prompt("What's your surname?");
-const userColor = prompt("What's your favorite color?");
+submitBtn.addEventListener("click", function(event){
+    event.preventDefault();
 
-suggestedPassword.innerHTML += userName + userSurname + userColor + "23";
+    const userName = document.getElementById("user-name").value;
+    const userSurname = document.getElementById("user-surname").value;
+    const userColor = document.getElementById("user-color").value;
+
+    suggestedPassword.innerHTML += userName + userSurname + userColor + "23";
+});
+
+//const userName = prompt("What's your name?");
+//const userSurname = prompt("What's your surname?");
+//const userColor = prompt("What's your favorite color?");
+
+//suggestedPassword.innerHTML += userName + userSurname + userColor + "23";
